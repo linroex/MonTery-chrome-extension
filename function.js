abef3acd3d5748bf70104bf29c84f70146c20662
linroex <linroex@coder.tw>
2015-05-23 14:49:20 +0800

    get location and convert to city ok

--- function.js
+++ function.js
@@ -8,3 +8,19 @@ function showNotification(title, message) {
 
     chrome.notifications.create('', options);
 }
+
+function getData() {
+    navigator.geolocation.getCurrentPosition(function(location){
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode({'latLng': new google.maps.LatLng(
+                location.coords.latitude, 
+                location.coords.longitude
+            )}, function(response, status){
+                if(status === google.maps.GeocoderStatus.OK) {
+                    console.log(response[0].address_components[4].long_name);
+                }
+        });
+    });
+    
+}
